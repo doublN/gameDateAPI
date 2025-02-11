4e49b43645844a4dc38a9db1bc8f444f5eca8f4a
doublN <n.newberry@hotmail.com>
2025-02-11 23:19:21 +0000

    add delete user endpoint

---
+++ src/controllers/user/deleteUser.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { object, string } from "yup";
+import { handleError } from "../../utils/error";
+import { UserService } from "../../services/userService";
+
+export class DeleteUserController {
+  async invoke(req: Request, res: Response) {
+    const requestValidation = object({
+      email: string().email().required(),
+      password: string().required(),
+    });
+
+    try {
+      const request = await requestValidation.validate(req.body, {
+        abortEarly: false,
+      });
+
+      const userService = new UserService();
+
+      const result = await userService.deleteUser(
+        request.email,
+        request.password
+      );
+
+      res.json({
+        success: result,
+      });
+    } catch (error) {
+      const response = handleError(error);
+      res.status(400);
+      res.json(response);
+    }
+  }
+}
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,11 +3,11 @@ import { CreateUserController } from "../controllers/user/createUser.js";
 import { LoginUserContoller } from "../controllers/user/loginUser.js";
 import { LogoutUserController } from "../controllers/user/logoutUser.js";
 import { checkSession } from "./middleware.js";
+import { DeleteUserController } from "../controllers/user/deleteUser.js";
 
 export const userRouter = Router();
 
 userRouter.post("/create", new CreateUserController().invoke);
 userRouter.post("/login", new LoginUserContoller().invoke);
 userRouter.delete("/logout", checkSession, new LogoutUserController().invoke);
-
-//delete
+userRouter.delete("/delete", checkSession, new DeleteUserController().invoke);
