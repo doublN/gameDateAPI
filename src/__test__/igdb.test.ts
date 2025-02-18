c8bf1a58917b67e31efe938188de98fbb1999cfb
doublN <n.newberry@hotmail.com>
2025-02-18 16:23:38 +0000

    add test for getGamesBySearch

--- src/__test__/igdb.test.ts
+++ src/__test__/igdb.test.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import "jest-extended";
 import { describe, expect, test } from "@jest/globals";
-import { credentials, fetchCredentials } from "../igdb";
+import { credentials, fetchCredentials, getGamesBySearch } from "../igdb";
 
 dotenv.config();
 
@@ -13,5 +13,22 @@ describe("fetch credentials", () => {
     expect(credentials.expires_in).toBeNumber();
     expect(credentials.token_type).toBeString();
     expect(credentials.token_type).toBe("bearer");
+  });
+});
+
+describe("getGamesBySearch", () => {
+  test("correctly fetches games", async () => {
+    const games = await getGamesBySearch("avowed");
+
+    expect(games).toBeArray();
+    expect(games[0]).toMatchObject({
+      id: 135994,
+      cover: {
+        id: 356807,
+        url: "//images.igdb.com/igdb/image/upload/t_thumb/co7nbb.jpg",
+      },
+      first_release_date: 1739836800,
+      name: "Avowed",
+    });
   });
 });
