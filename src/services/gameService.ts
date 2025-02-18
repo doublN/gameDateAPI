697cf24309cd7014abdb6bbcdac9d72e0eda5f4e
doublN <n.newberry@hotmail.com>
2025-02-18 16:27:38 +0000

    create gameService

---
+++ src/services/gameService.ts
@@ -0,0 +1,17 @@
+import { getGamesBySearch } from "../igdb";
+
+export class GameService {
+  async searchGame(query: string) {
+    const games = await getGamesBySearch(query);
+
+    return games.map((game) => {
+      return {
+        ...game,
+        cover: {
+          ...game.cover,
+          url: game.cover?.url.replace("t_thumb", "t_cover_small"),
+        },
+      };
+    });
+  }
+}
