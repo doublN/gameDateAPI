9501a7a4c8311d81f039b7b0dc0282b5226c3863
doublN <n.newberry@hotmail.com>
2025-02-26 17:29:32 +0000

    add getGameList method to list service to get the users game list

--- src/services/listService.ts
+++ src/services/listService.ts
@@ -1,8 +1,16 @@
+import { FieldPacket } from "mysql2";
 import { query } from "../db/db.js";
 import { signedInUser } from "../global.js";
 import { getGameById } from "../igdb/index.js";
 import { CoverService } from "./coverService.js";
 import { GameService } from "./gameService.js";
+
+export type GameListGame = {
+  id: number;
+  cover: string;
+  name: string;
+  firstReleaseDate: number;
+};
 
 export class ListService {
   gameService = new GameService();
@@ -10,7 +18,7 @@ export class ListService {
 
   async addGameToList(gameId: number) {
     if (!signedInUser.id) {
-      throw new Error("No user id");
+      throw new Error("User not set");
     }
 
     const game = await getGameById(gameId);
@@ -25,5 +33,24 @@ export class ListService {
       signedInUser.id,
       game.id,
     ]);
+  }
+
+  async getGameList(userId: number): Promise<Array<GameListGame>> {
+    const [games] = (await query(
+      "SELECT games.id, games.firstReleaseDate, games.name, covers.url as cover FROM list INNER JOIN games ON list.gameId = games.id INNER JOIN covers ON covers.id = games.cover WHERE list.userId = ? ORDER BY games.firstReleaseDate ASC",
+      [userId]
+    )) as [GameListGame[], FieldPacket[][]];
+
+    if (Array.isArray(games) && games.length > 0) {
+      return games.map((game) => {
+        return {
+          ...game,
+          firstReleaseDate: game.firstReleaseDate * 1000,
+          cover: game.cover.replace("t_thumb", "t_cover_big"),
+        };
+      });
+    }
+
+    return [];
   }
 }
